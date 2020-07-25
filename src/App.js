46dc9b3e4a1c68d709ca926a298294b475bcce74
Emily Carlisle <emilydeluna96@gmail.com>
2020-07-25 13:42:44 -0500

    pseudocoding

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Container from "./components/Container";
 
+// returns the Container components that have the navbar, table components and API call
 function App() {
   return <Container/>
 }
